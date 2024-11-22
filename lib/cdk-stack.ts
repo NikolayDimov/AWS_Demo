86faebea8d41a0e875d311c34cc24c87fc3c3a6d
Nikolay Dimov <nikolay.dimov@yara.com>
2024-11-22 23:15:20 +0200

    lib/cdk-stack -- add dynamoDB table, API

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,35 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // DynamoBD Table creation
+        const errorTable: Table = new Table(this, "ErrorTable", {
+            partitionKey: {
+                name: "id",
+                type: AttributeType.STRING,
+            },
+            billingMode: BillingMode.PAY_PER_REQUEST,
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // API Gateway
+        // function
+        const processFunction = new NodejsFunction(this, "processFunction", {
+            runtime: Runtime.NODEJS_20_X,
+            handler: "handler",
+            entry: `${__dirname}/../src/processFunction.ts`,
+        });
+
+        // api
+        const api = new RestApi(this, "ProcessorApi");
+        const resource = api.root.addResource("processJSON");
+        resource.addMethod("POST", new LambdaIntegration(processFunction));
+    }
 }
